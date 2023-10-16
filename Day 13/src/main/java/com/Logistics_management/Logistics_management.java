51f7a9c0b83f1573ff6067e70132ad3589b57384
harishj07 <119096518+harishj07@users.noreply.github.com>
2023-10-16 15:21:18 +0530

    Add files via upload

---
+++ Day 13/src/main/java/com/Logistics_management/Logistics_management.java
@@ -0,0 +1,13 @@
+package com.Logistics_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Logistics_management {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Logistics_management.class, args);
+	}
+
+}
---
+++ Day 13/src/main/java/com/Logistics_management/models/ERole.java
@@ -0,0 +1,7 @@
+package com.Logistics_management.models;
+
+public enum ERole {
+  ROLE_USER,
+  ROLE_EMPLOYEE,
+  ROLE_ADMIN
+}
